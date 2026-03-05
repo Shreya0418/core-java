e075b1a466f46f8086e4cdee0895e9c5af9d494d
Shreya0418 <shreyarh18@gmail.com>
2026-03-05 10:49:21 +0530

    examples of array

---
+++ Arrays/Lays.java
@@ -0,0 +1,44 @@
+class Lays {
+	
+	public static void main(String[] args) {
+		
+		String productName = "Lay's - India's Magic Masala";
+		
+		String ingridents[] = {"Potato", 
+		"Edible Vegetable Oil : (Palmolein Oil, Rice Bran Oil)", 
+		"Spices & Condiments : (Onion Powder, Chilli Powder, Ginger Powder, Garlic Powder, Black Pepper Powder, Dried Mango Powder, Coriander Seed Powder, Spices Extract, Turmeric Powder)", 
+		"Salt", 
+		"Black Salt", 
+		"Tomato Powder", 
+		"Citric Acid(INS 300)"};
+		
+		// System.out.println("The Flavoured Lay's is : ");
+		// System.out.println(ingridents[0] + "\n" + ingridents[1] + "\n" + ingridents[2] + "\n" + ingridents[3] + "\n" + ingridents[4] + "\n" + ingridents[5] + "\n" + ingridents[6]);
+		
+		System.out.println("The Flavoured Lay's is : ");
+
+   String i1 = ingridents[0];  
+   System.out.println(i1);
+
+   String i2 = ingridents[1];  
+   System.out.println(i2);
+
+  String i3 = ingridents[2];  
+  System.out.println(i3);
+
+  String i4 = ingridents[3];  
+ System.out.println(i4);
+
+  String i5 = ingridents[4];  
+ System.out.println(i5);
+
+    String i6 = ingridents[5];  
+System.out.println(i6);
+
+  String i7 = ingridents[6];  
+System.out.println(i7);
+
+		
+	}
+
+}
