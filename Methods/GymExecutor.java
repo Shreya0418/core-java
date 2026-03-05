faa653b403fe998400b23194d074e3a345c59aa1
Shreya0418 <shreyarh18@gmail.com>
2026-03-05 10:54:22 +0530

    Examples of variables

--- Methods/GymExecutor.java
+++
@@ -1,9 +0,0 @@
-class GymExecutor {
-	
-	public static void main(String[] gym) {
-		System.out.println("Main Method Started");
-		
-		Gym.getEquipmentNames();
-		System.out.println("\nMain Method Ends");
-	}
-}
