180a6497dce49a3764f50d64cd3311f799fe7459
Shreya0418 <shreyarh18@gmail.com>
2026-03-23 11:43:46 +0530

    Examples of method using switch condition

---
+++ Contacts.java
@@ -0,0 +1,478 @@
+class Contacts {
+	
+	static long searchContactNumbersByName(String name)  {
+		
+		long number = 00;
+		switch(name){ 
+			
+		case "Shreya":
+		System.out.println("case1") ;
+		    return 8217216186L;
+			
+		case "Ziya":
+		System.out.println("case2") ;
+		    return 8088642008L;
+				
+		case"Pavan" :
+		    return 8088642008L;
+			
+		case"Pranati" :
+			return 7411212104L;
+			
+		case"Mommy":
+			return 9964571405L;
+			
+		case"Pappa":
+			return 9481728156L;
+			
+		case"Asfiya di" :
+			return 9538342626L;
+			
+		case"Koustubh" :
+			return 7338610392L;
+			
+		case"Shivani" :
+			return 8431749699L;
+			
+		case"Monika" :
+			return 9110818086L;
+			
+		case"Shree" :
+			return 8296897154L;
+			
+		case"Pratii" :
+			return 9972142278L;
+			
+		case"Deepu" :
+			return 9611589556L;
+			
+		case"Atte Sirsi" :
+			return 9632621425L;
+			
+		case"Tatha" :
+			return 6362412845L;
+			
+		case"Kumar Kaka" :
+			return 9740071077L;
+	    
+		case"Vidyaa" :
+			return 6360697975L;
+			
+		case"Aishu" :
+			return 9380491536L;
+			
+		case"Bhanu" :
+			return 6361359375L;
+			
+		case"Sushma" :
+			return 6363889680L;
+			
+		case"Srujana" :
+			return 9538462889L;
+			
+		case"Sunil" :
+			return 8217353194L;
+			
+		case"Karna" :
+			return 9148052308L;
+			
+		case"Kavya Akka" :
+			return 8310483983L;
+			
+		case"Keshav" :
+			return 8310075557L;
+			
+		case"Aadii" :
+			return 8147389463L;
+			
+		case"Prakash Kaka" :
+			return 9060959377L;
+			
+		case"Pooja aunty" :
+			return 8660084963L;
+			
+		case"Poornima Aunty" :
+			return 8884811316L;
+			
+		case"Doddapa" :
+			return 9844240898L;
+			
+		case"Krupa" :
+			return 8431745515L;
+			
+		case"Shilpa" :
+			return 9113909987L;
+			
+		case"Spandana" :
+			return 9902323404L;
+			
+		case"Shraddha" :
+			return 8088561194L;
+			
+		case"Siri" :
+			return 9611207429L;
+	    
+		case"Misha" :
+			return 9036315166L;
+			
+		case"Rakshita" :
+			return 6364008208L;
+			
+		case"Vishaka" :
+			return 7829277628L;
+			
+		case"Varsha" :
+			return 7892908922L;
+			
+		case"Sruthi" :
+			return 8431978249L;
+			
+		case"Vinayak" :
+			return  8792594914L;
+			
+		case"Srusti" :
+			return 7483399539L;
+			
+		case"Harsha" :
+			return 9353241569L;
+			
+		case"Sanjeev" :
+			return 8660532392L;
+			
+		case"Navya" :
+			return 8095540529L;
+			
+		case"Mansi" :
+			return 7019794776L;
+			
+		case"Kavu" :
+			return 8867279544L;
+			
+		case"Sneha" :
+			return 9110831070L;
+			
+		case"Shrinidhi" :
+			return 9353705100L;
+			
+		case"Shaheen" :
+			return 9778259738L;
+			
+		case"Poornima" :
+			return 8904552402L;
+			
+		case"Mithali" :
+			return 8147765751L;
+			
+		case"Arpita" :
+			return 7483688900L;
+			
+		case"Prakruthi" :
+			return 6366180352L;
+	    
+		case"Khushi" :
+			return 8088169553L;
+			
+		case"Anush" :
+			return 7483418371L;
+			
+		case"PR Harshita" :
+			return 9110208594L;
+			
+		case"Shalini" :
+			return 7019225248L;
+			
+		case"Sujal" :
+			return 8431098300L;
+			
+		case"Ajji" :
+			return 9740203182L;
+			
+		case"Ananya" :
+			return 7204730296L;
+			
+		case"Ankitha" :
+			return 9110296022L;
+			
+		case"Anwar Uncle" :
+			return 9986786430L;
+			
+		case"Divya" :
+			return 6362755385L;
+			
+		case"Auntyy" :
+			return 9731709866L;
+			
+		case"Chirag" :
+			return 8660570504L;
+			
+		case"Chatur" :
+			return 9611885208L;
+			
+		case"Darshan" :
+			return 8431416159L;
+				
+		case"Gagana" :
+			return 6362796033L;
+			
+		case"Geeta" :
+			return 9380554082L;
+			
+		case"Shakuntala" :
+			return 8660731658L;
+			
+		case"Gouri" :
+			return 9604176446L;
+	    
+		case"Harshita" :
+			return 7892518833L;
+			
+		case"Hamika" :
+			return 7795346599L;
+			
+		case"Hersheys" :
+			return 7013545431L;
+			
+		case"Honey" :
+			return 8618096749L;
+			
+		case"Sunita" :
+			return 9493248221L;
+			
+		case"Iqrabanu" :
+			return 9448745454L;
+			
+		case"Ishrat" :
+			return 7892256780L;
+			
+		case"Kannika" :
+			return 7892185114L;
+			
+		case"Kallapur Atte" :
+			return 8660413175L;
+			
+		case"Kavana" :
+			return 8660643320L;
+			
+		case"Kavya Kundgol" :
+			return 6361639410L;
+			
+		case"Shrusti" :
+			return 9110691723L;
+			
+		case"Shrusta" :
+			return 7406270942L;
+			
+		case"Koushallya" :
+			return 9740994250L;
+			
+		case"Kruthika" :
+			return 7892345542L;
+			
+		case"Lohith" :
+			return 8618487480L;
+			
+		case"Madhura" :
+			return 8660282611L;
+			
+		case"Mamatha" :
+			return 7676159755L;
+			
+		case"Manu" :
+			return 7022916180L;
+	    
+		case"Megha" :
+			return 9480526714L;
+			
+		case"MMD" :
+			return 9663946789L;
+			
+		case"Nandini" :
+			return 9008929674L;
+			
+		case"Nanditha" :
+			return 9481727145L;
+			
+		case"Tanuja" :
+			return 6363332768L;
+			
+		case"Neha" :
+			return 9482233219L;
+			
+		case"Parvathi" :
+			return 7483264457L;
+			
+		case"Nikath" :
+			return 8088778749L;
+			
+		case"Pooja" :
+			return 8197537100L;
+			
+		case"Pragati" :
+			return 9738488548L;
+			
+		case"Prashant Uncle" :
+			return 8088616186L;
+			
+		case"Pratham" :
+			return 6360113636L;
+			
+		case"Pratiksha" :
+			return 9449514082L;
+			
+		case"Priya" :
+			return  8792855107L;
+			
+		case"Veena Dodamma" :
+			return 9844997053L;
+			
+		case"Toya Aunty" :
+			return 8277227717L;
+			
+		case"Dinesh Kaka" :
+			return 9449454443L;
+			
+		case"Pruthvi" :
+			return 7676775371L;
+			
+		case"Raghu Mama" :
+			return 9945348404L;
+	    
+		case"Punya" :
+			return 7483770373L;
+			
+		case"Rakshita Kumari" :
+			return 7892444892L;
+			
+		case"Ramya" :
+			return 9019113750L;
+			
+		case"Ranjana" :
+			return 8792459694L;
+			
+		case"Tanzilla" :
+			return 8073033915L;
+			
+		case"Ratna" :
+			return 9663922986L;
+			
+		case"Riya" :
+			return 6362512351L;
+			
+		case"Rishi" :
+			return 8147728519L;
+			
+		case"Rohan" :
+			return 7676244406L;
+			
+		case"Rupesh" :
+			return 8904850580L;
+			
+		case"Sachin" :
+			return 8867550600L;
+			
+		case"Sagar" :
+			return 9620295446L;
+			
+		case"Sahal" :
+			return 8431630905L;
+			
+		case"Sahana" :
+			return 9449381592L;
+			
+		case"Sakshi" :
+			return 8123285949L;
+			
+		case"Sandhya" :
+			return 9606796547L;
+			
+		case"Saniya" :
+			return 8971357960L;
+			
+		case"Santaba Athya" :
+			return 9482177588L;
+			
+		case"Shankramma" :
+			return 9740831856L;
+	    
+		case"Sharan" :
+			return 9035897322L;
+			
+		case"Shilpa Mam" :
+			return 9902288446L;
+			
+		case"Shilpa Shinde" :
+			return 8762271411L;
+			
+		case"Shoba" :
+			return 9844733487L;
+			
+		case"Varshaa" :
+			return 7019665247L;
+			
+		case"Shravana" :
+			return 9663193053L;
+			
+		case"Shravya" :
+			return 9148918535L;
+			
+		case"Shreekant" :
+			return 9483067961L;
+			
+		case"Shreenath sir" :
+			return 8147751569L;
+			
+		case"Shreya NMH" :
+			return 7619551521L;
+			
+		case"Shrii Mama" :
+			return 9008070589L;
+			
+		case"Shrusti N" :
+			return 9844205685L;
+			
+		case"Shwetha" :
+			return 9591322602L;
+			
+		case"Simran" :
+			return 7899527570L;
+			
+		case"Sinchana" :
+			return 7975734041L;
+			
+		case"Sindhu" :
+			return 7975263151L;
+			
+		case"Sister Tresa" :
+			return 7349163114L; 
+			
+		case"Sooraj" :
+			return 9353827580L;
+			
+		case"Soumya" :
+			return 7411031008L;
+	    
+		case"Spoorthi" :
+			return 9148318137L;
+			
+		case"Zubeda aunty" :
+			return 8073991583L;
+			
+		case"Yashaswini" :
+			return 8296693247L;
+			
+			
+		
+		default :
+	    return 0;
+		
+			
+		
+
+		}
+		
+		
+		
+		
+	}
+
+}
